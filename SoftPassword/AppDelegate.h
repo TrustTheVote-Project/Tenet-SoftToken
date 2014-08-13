c2a30a1cf8cbe6f6104bb55b344bf3344fc2083b
Aleksey Gureiev <spyromus@noizeramp.com>
2014-08-13 16:24:23 +0400

    Initial Commit

---
+++ SoftPassword/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SoftPassword
+//
+//  Created by Aleksey Gureiev on 8/13/14.
+//  Copyright (c) 2014 TrustTheVote. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ SoftPassword/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SoftPassword
+//
+//  Created by Aleksey Gureiev on 8/13/14.
+//  Copyright (c) 2014 TrustTheVote. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
